6b51196b93279dcf999899d9078f45c6cad09bfb
atti199 <atti199@gmail.com>
2021-11-10 22:13:30 +0100

    init

--- Radiohead.R
+++ Radiohead.R
@@ -13,3 +13,16 @@ access_token <- get_spotify_access_token()
 beatles <- get_artist_audio_features('the beatles')
 radiohead <- get_artist_audio_features('Radiohead')
 # radiohead2 <- get_artist("4Z8W4fKeB5YxbusRsdQVPb")
+library(lubridate)
+
+get_my_recently_played(limit = 5) %>% 
+  mutate(artist.name = map_chr(track.artists, function(x) x$name[1]),
+         played_at = as_datetime(played_at)) %>% 
+  select(track.name, artist.name, track.album.name, played_at) %>% 
+  kable()
+get_my_top_artists_or_tracks(type = 'artists', time_range = 'long_term', limit = 5) %>% 
+  select(name, genres) %>% 
+  rowwise %>% 
+  mutate(genres = paste(genres, collapse = ', ')) %>% 
+  ungroup %>% 
+  kable()
