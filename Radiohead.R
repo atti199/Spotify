e22380941923bc0914385c148ded85bee44aab18
atti199 <atti199@gmail.com>
2021-11-10 22:01:03 +0100

    init

---
+++ Radiohead.R
@@ -0,0 +1,15 @@
+# install.packages("devtools")
+# devtools::install_github('charlie86/spotifyr')
+# install.packages("spotifyr")
+# install.packages("tidyverse")
+library(spotifyr)
+library(tidyverse)
+library(knitr)
+Sys.setenv(SPOTIFY_CLIENT_ID = '196e96f8a0c6493fb713c2a2c41757e6')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'c23af2ddc61e464e8e95b21406010a50')
+
+access_token <- get_spotify_access_token()
+
+beatles <- get_artist_audio_features('the beatles')
+radiohead <- get_artist_audio_features('Radiohead')
+# radiohead2 <- get_artist("4Z8W4fKeB5YxbusRsdQVPb")
