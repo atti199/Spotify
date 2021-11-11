1577d8735ca56b629785b30bb03e2dfae225499f
atti199 <atti199@gmail.com>
2021-11-11 18:50:22 +0100

    init

--- Radiohead.R
+++ Radiohead.R
@@ -6,23 +6,21 @@ library(spotifyr)
 library(tidyverse)
 library(knitr)
 Sys.setenv(SPOTIFY_CLIENT_ID = '196e96f8a0c6493fb713c2a2c41757e6')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = 'c23af2ddc61e464e8e95b21406010a50')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '4866d222b15a47c0a83fdff5d47fb653')
 
-access_token <- get_spotify_access_token()
+library (spotifyr)
+
+access_token <- get_spotify_access_token(
+)
 
 beatles <- get_artist_audio_features('the beatles')
 radiohead <- get_artist_audio_features('Radiohead')
 # radiohead2 <- get_artist("4Z8W4fKeB5YxbusRsdQVPb")
-library(lubridate)
 
-get_my_recently_played(limit = 5) %>% 
-  mutate(artist.name = map_chr(track.artists, function(x) x$name[1]),
-         played_at = as_datetime(played_at)) %>% 
-  select(track.name, artist.name, track.album.name, played_at) %>% 
-  kable()
-get_my_top_artists_or_tracks(type = 'artists', time_range = 'long_term', limit = 5) %>% 
-  select(name, genres) %>% 
-  rowwise %>% 
-  mutate(genres = paste(genres, collapse = ', ')) %>% 
-  ungroup %>% 
-  kable()
+variable.names(spotify_tracks)
+# df <- spotify_tracks <- read_csv("~/Documents/R/felmero/Spotify/SpotGenTrack/Data Sources/spotify_tracks.csv")
+df <- spotify_tracks[c("popularity", "energy", "key", "loudness", "mode", "tempo", "valence", "acousticness", "danceability", "instrumentalness", "liveness", "speechiness" ,"time_signature")]
+sample <- df[sample(nrow(df), 3000), ]
+
+m1 <- lm(data = sample, popularity ~ energy + key + loudness + mode + tempo + valence + acousticness + danceability + instrumentalness + liveness + speechiness + time_signature)
+summary(m1)
